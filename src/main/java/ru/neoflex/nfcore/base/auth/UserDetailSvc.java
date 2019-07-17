543c939afbeb3face3f09627b363d84e269893ff
hedaira <mezzo666@mail.ru>
2019-07-17 17:55:15 +0300

    add ldap

--- src/main/java/ru/neoflex/nfcore/base/auth/UserDetailSvc.java
+++ src/main/java/ru/neoflex/nfcore/base/auth/UserDetailSvc.java
@@ -1,6 +1,5 @@
 package ru.neoflex.nfcore.base.auth;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
