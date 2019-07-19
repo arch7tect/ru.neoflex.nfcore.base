d3de7f680352fedc691163666333fa8010ec571e
hedaira <mezzo666@mail.ru>
2019-07-19 12:21:54 +0300

    Correct imports Correct api (auth)

--- src/main/java/ru/neoflex/nfcore/base/controllers/SysController.java
+++ src/main/java/ru/neoflex/nfcore/base/controllers/SysController.java
@@ -1,7 +1,7 @@
 package ru.neoflex.nfcore.base.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
@@ -10,8 +10,7 @@ import java.security.Principal;
 @RequestMapping("/system")
 public class SysController {
 
-    @RequestMapping(value="/user", produces = "application/json; charset=utf-8")
-    @ResponseBody
+    @GetMapping(value="/user", produces = "application/json; charset=utf-8")
     public Principal getUser(Principal principal) {
         return principal;
     }
